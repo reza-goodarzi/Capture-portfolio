a1290861d77498d66daaac8e4673a2481ae386e1
Reza Goodarzi <rezagoodarzi026@gmail.com>
2021-04-23 07:00:59 +0430

    AboutUs Section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,12 @@
+// Page components
+import AboutSection from "../components/AboutSection";
+
+const AboutUs = () => {
+	return (
+		<div>
+			<AboutSection />
+		</div>
+	)
+}
+
+export default AboutUs

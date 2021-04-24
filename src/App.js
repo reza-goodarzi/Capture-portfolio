e482e2e6d5238a6739f491fddac988ca469f5f7c
Reza Goodarzi <rezagoodarzi026@gmail.com>
2021-04-24 12:22:30 +0430

    nav and React-Router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import AboutUs from "./pages/AboutUs";
 
 // Global Style
 import GlobalStyle from "./components/GlobalStyle";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+
+// Router
+import { Switch, Route } from 'react-router-dom';
+import Nav from "./components/Nav";
 
 
 
@@ -10,7 +16,22 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+
+      <Switch>
+        <Route exact path="/" >
+          <AboutUs />
+        </Route>
+
+        <Route exact path="/work">
+          <OurWork />
+        </Route>
+
+        <Route exact path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
+      
     </div>
   );
 }

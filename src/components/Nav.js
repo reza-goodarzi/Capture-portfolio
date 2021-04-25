c3cdd025b8594df6ea463a5d5e8607771f11d7cd
Reza Goodarzi <rezagoodarzi026@gmail.com>
2021-04-25 10:32:41 +0430

    Nav Animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { Link, useLocation } from "react-router-dom";
 import styled from 'styled-components';
+
 const Nav = () => {
+	const { pathname } = useLocation();
 	return (
 		<StyledNav>
 			<h1><Link to="/" id="logo" href="#">Capture</Link></h1>
@@ -8,12 +11,27 @@ const Nav = () => {
 			<ul>
 				<li>
 					<Link to="/">1. About Us</Link>
+					<Line
+						transition={{ duration: .75 }}
+						initial={{ width: 0 }}
+						animate={{ width: pathname === '/' ? '50%' : 0 }}
+					/>
 				</li>
 				<li>
 					<Link to="/work">2. Our Work</Link>
+					<Line
+						transition={{ duration: .75 }}
+						initial={{ width: 0 }}
+						animate={{ width: pathname === '/work' ? '50%' : 0 }}
+					/>
 				</li>
 				<li>
 					<Link to="/contact">3. Contact Us</Link>
+					<Line
+						transition={{ duration: .75 }}
+						initial={{ width: 0 }}
+						animate={{ width: pathname === '/contact' ? '50%' : 0 }}
+					/>
 				</li>
 			</ul>
 		</StyledNav>
@@ -52,6 +70,15 @@ const StyledNav = styled.nav`
 		padding-left: 10rem;
 		position: relative;
 	}
+`;
+
+const Line = styled(motion.div)`
+	height: .3rem;
+	background: #23d997;
+	width: 0%;
+	position: absolute;
+	bottom: -80%;
+	left: 60%;
 `;
 
 export default Nav;
